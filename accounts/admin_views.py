e81693bce96a007765936984f0b6f8aa73654181
AcMongue <aimecharlesmongue@gmail.com>
2026-01-11 18:36:31 +0100

    FIX admin correction: desactiver signal pendant creation Expense

--- accounts/admin_views.py
+++ accounts/admin_views.py
@@ -5,6 +5,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.db import transaction as db_transaction
+from django.db.models.signals import post_save
 from .models import WalletTransaction
 from expenses.models import Expense
 
@@ -16,6 +17,9 @@ def fix_wallet_expenses_view(request):
     Accessible uniquement aux admins via /admin/fix-wallet-expenses/
     """
     if request.method == 'POST':
+        # Importer le signal ici pour le désactiver temporairement
+        from .wallet_models import create_or_update_wallet_transaction_from_expense
+        
         # Exécuter la correction
         transactions_without_expense = WalletTransaction.objects.filter(
             transaction_type='expense',
@@ -32,25 +36,32 @@ def fix_wallet_expenses_view(request):
         for trans in transactions_without_expense:
             try:
                 with db_transaction.atomic():
-                    expense = Expense.objects.create(
-                        user=trans.wallet.user,
-                        amount=trans.amount,
-                        category=trans.category,
-                        description=trans.description,
-                        date=trans.date
-                    )
+                    # Désactiver le signal temporairement pour éviter la création d'une 2e transaction
+                    post_save.disconnect(create_or_update_wallet_transaction_from_expense, sender=Expense)
                     
-                    trans.expense = expense
-                    trans.save()
-                    
-                    results['created'] += 1
-                    results['details'].append({
-                        'transaction_id': trans.id,
-                        'expense_id': expense.id,
-                        'amount': str(trans.amount),
-                        'description': trans.description,
-                        'user': trans.wallet.user.username
-                    })
+                    try:
+                        expense = Expense.objects.create(
+                            user=trans.wallet.user,
+                            amount=trans.amount,
+                            category=trans.category,
+                            description=trans.description,
+                            date=trans.date
+                        )
+                        
+                        trans.expense = expense
+                        trans.save()
+                        
+                        results['created'] += 1
+                        results['details'].append({
+                            'transaction_id': trans.id,
+                            'expense_id': expense.id,
+                            'amount': str(trans.amount),
+                            'description': trans.description,
+                            'user': trans.wallet.user.username
+                        })
+                    finally:
+                        # Réactiver le signal
+                        post_save.connect(create_or_update_wallet_transaction_from_expense, sender=Expense)
                     
             except Exception as e:
                 results['errors'].append({
